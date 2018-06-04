0d65c74a8235e51f07e13249df1a6923b922fcc4
Felipe <lippee@gmail.com>
2018-06-03 22:59:21 -0300

    tokenring funcional (falta colocar comentários e refatorar nomes)

--- src/tokenring/Server.java
+++ src/tokenring/Server.java
@@ -3,37 +3,36 @@ package tokenring;
 import java.io.*;
 import java.net.*;
 
-public class Server implements Runnable
-{
-    Socket socket=null;
+public class Server implements Runnable {
+    
+	Socket socket = null;
     static ServerSocket ss;
-    Server(Socket newSocket)
-    {
-        this.socket=newSocket;        
+    
+    public Server(Socket newSocket) {
+        this.socket = newSocket;        
     }
-    public static void main(String args[]) throws IOException
-    {
-        ss=new ServerSocket(7000);
-        System.out.println("Server Started");
-        while(true)
-        {
+    
+    public static void main(String args[]) throws IOException {
+        
+    	ss = new ServerSocket(7000);
+        
+    	System.out.println("Servidor Iniciado");
+        
+    	while(true) {
             Socket s = ss.accept();
             Server es = new Server(s);
             Thread t = new Thread(es);
             t.start();
         }
     }
-    public void run()
-    {
+    
+    public void run() {
         try {        
-            BufferedReader in = new BufferedReader(new 
-InputStreamReader(socket.getInputStream()));
-            while(true)
-            {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            while(true) {
                 System.out.println(in.readLine());
             }
-        }
-        catch(Exception e)
-        {}
+        } catch (Exception e) {}
     }
 }
