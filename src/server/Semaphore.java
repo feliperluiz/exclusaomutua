3f76a2720716a8012d5370564758afb2a749ae7b
Felipe <lippee@gmail.com>
2018-06-07 19:38:45 -0300

    FIFO Semaphore strategy

---
+++ src/server/Semaphore.java
@@ -0,0 +1,26 @@
+package server;
+
+public class Semaphore {
+	private int value;
+
+	public Semaphore(int value) {
+		this.value = value;
+	}
+
+	public synchronized void acquire() {
+		while (value <= 0) {
+			try {
+				wait();
+			}
+			catch (InterruptedException e) { }
+		}
+
+		value--;
+	}
+
+	public synchronized void release() {
+		++value;
+
+		notify();
+	}
+}
